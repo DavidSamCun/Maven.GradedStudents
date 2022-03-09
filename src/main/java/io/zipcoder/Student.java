840923dfae9e569614fa3b6c0def6abbc98030d5
DavidCun <99755247+DavidSamCun@users.noreply.github.com>
2022-03-09 10:17:15 -0500

    forgot2branch

--- src/main/java/io/zipcoder/Student.java
+++ src/main/java/io/zipcoder/Student.java
@@ -1,4 +1,31 @@
 package io.zipcoder;
 
+import java.util.ArrayList;
+
 public class Student {
+
+    private String firstName;
+    private String lastName;
+    private ArrayList<Double> examScores = new ArrayList<>();
+
+
+    public Student(){
+
+        this.firstName = "First Name";
+        this.lastName = "Last Name";
+
+    }
+
+    public Student(String firstName, String lastName){
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public void addScore(Double score){
+
+        examScores.add(score);
+    }
+
+
+
 }
