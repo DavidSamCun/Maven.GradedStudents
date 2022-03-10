3a010c2d779f042cfba5d8b9876f9ec179b1d56e
DavidCun <99755247+DavidSamCun@users.noreply.github.com>
2022-03-10 17:45:50 -0500

    update

--- src/main/java/io/zipcoder/Classroom.java
+++ src/main/java/io/zipcoder/Classroom.java
@@ -1,5 +1,7 @@
 package io.zipcoder;
 
+import java.util.ArrayList;
+
 public class Classroom {
 
     private Student[] students;
@@ -28,6 +30,16 @@ public class Classroom {
         }
         average /= students.length;
         return average;
+   }
+
+   public void addStudent(Student newStudent){
+        for(int i = 0; i < students.length; i++){
+            if(students[i] != null){
+                students[i] = newStudent;
+            } else {
+                System.out.println("We're full");
+            }
+        }
    }
 
 }
