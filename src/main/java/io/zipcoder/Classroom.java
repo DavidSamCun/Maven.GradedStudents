32955f41ff45a28dc33da3e3a8b7bad6974b240d
DavidCun <99755247+DavidSamCun@users.noreply.github.com>
2022-03-09 20:25:17 -0500

    stukk

--- src/main/java/io/zipcoder/Classroom.java
+++ src/main/java/io/zipcoder/Classroom.java
@@ -1,4 +1,26 @@
 package io.zipcoder;
 
 public class Classroom {
+
+    private Student[] students;
+
+    public Classroom(){
+        Student[] students = new Student[30];
+    }
+
+    public Classroom(Student[] students){
+        this.students = students;
+    }
+
+    public Classroom(int max){
+        Student[] student = new Student[max];
+    }
+
+//    public Student getStudents(){
+//
+//        return Student;
+//    }
+
+
+
 }
